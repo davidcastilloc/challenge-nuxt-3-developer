4ea77f80d5e24bc3f08bc6933198f88ac1e5008c
David Castillo <vikruzdavid@gmail.com>
2024-11-10 04:57:05 -0400

    Fix Quote of the day to new refactor.

--- components/App/stores/QuoteOfTheDay.vue
+++ components/App/stores/QuoteOfTheDay.vue
@@ -5,18 +5,40 @@
         Quote of the day
       </h2>
       <h5 class="subtitle">
-        {{ quote.quote }}
+        {{ data.quote }}
       </h5>
       <p class="text-sm">
-        {{ quote.author }}
+        {{ data.author }}
       </p>
     </BaseProse>
   </BaseMessage>
 </template>
 
 <script lang="ts" setup>
-const config = useRuntimeConfig()
-const { data: quote } = await useAsyncData('quote-of-the-day', () => $fetch('/api/quote'), { server: true })
+// get quote from server 24h cache
+const { data } = await useAsyncData('quote-of-the-day', () => $fetch('/api/quote'), {
+  transform: (input: any) => ({
+    ...input,
+    fetchedAt: Date.now(),
+  }),
+  getCachedData: (key, nuxtApp) => {
+    const quoteCached =  nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+    // Check if the quote is cached
+    if (!quoteCached) {
+      return
+    }
+    // Check if the quote is expired
+    const DAY = 24 * 60 * 60 * 1000 // 24 hours in ms
+    const expiration = new Date(quoteCached.fetchedAt);
+    expiration.setTime(expiration.getTime() + DAY);
+    const isExpired = expiration.getTime() < Date.now();
+    // If the quote is expired, return undefined
+    if (isExpired) {
+      return
+    }
+    return quoteCached
+  },
+})
 </script>
 
 <style></style>
